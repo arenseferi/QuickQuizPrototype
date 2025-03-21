aab1d8c7f192070487d3b2676f026ea667c84285
aren <arenseferi@outlook.com>
2025-03-21 09:22:43 +0100

    Fix: Router basename and Vite base path

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Quiz from './pages/Quiz';
-import Results from './pages/Results';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
+import Results from "./pages/Results";
 
 function App() {
   return (
-    <BrowserRouter basename="/QuickQuizPrototype">
+    <Router basename="/">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/results" element={<Results />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
